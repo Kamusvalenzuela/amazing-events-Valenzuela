624c0a5c7c0329349410e8bb4cc041de48892f92
Kamus <gunz_franco@hotmail.com>
2023-02-27 07:54:10 -0300

    task 2 finalizada
    Tras una sobredosis de café, salió esta maravilla

---
+++ javascript/details.js
@@ -0,0 +1,30 @@
+
+const currentDate = data.currentDate;
+const dataEvents = data.events;
+const info = dataEvents
+const containerDescription = document.getElementById("container-details")
+const containerImage = document.getElementById("image-details")
+const queryString = location.search
+const params = new URLSearchParams(queryString).get("id")
+const idNumber = parseInt(params) 
+
+
+const events = dataEvents.find(item => item._id === idNumber)
+
+
+containerDescription.innerHTML = `
+            <h1>${events.name}</h1>
+            <p>${events.category}</p>
+            <p>${events.description}</p>
+            <p>Place: ${events.place}</p>
+            <p>Capacity: ${events.capacity}</p>
+            <p>Price: $${events.price}</p>
+    `
+
+
+
+containerImage.innerHTML = `
+            <div class="d-flex w-100 h-100" >
+                <img class="" src="${events.image}" alt="this an image of ${events.name}"/>      
+            </div>
+    `
